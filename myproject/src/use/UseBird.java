8b98c49a5a0d62cf3bacd91256e71ccabae6506d
dlfjs0472 <dlfjs0472@gmail.com>
2021-04-29 12:57:31 +0900

    자바3일차

---
+++ myproject/src/use/UseBird.java
@@ -0,0 +1,10 @@
+package use;
+import animal.Bird;//클래스패스 루트, 경로 밑에서부터 적자
+			//
+class UseBird{
+	public static void main(String[] args){
+		//나와는 다른 경로에 있는 클래스이 Bird를 사용해본다!!
+		Bird b = new Bird();
+		b.fly();
+	}
+}
